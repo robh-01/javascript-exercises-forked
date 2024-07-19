0eec6aa07f7906fdfb637fdd5f7391f66f901acc
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-07-19 14:31:22 +0545

    Complete top's javascript-exercises -> 02,03,04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...elements) {
+    let filteredArray = [];
+    for (i = 0; i < array.length; i++) {
+        if (!elements.includes(array[i])) { 
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
