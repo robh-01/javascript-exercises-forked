fcb95ece63b267a2d2c45f2901fb63a8a61e023b
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2024-07-19 18:11:23 +0545

    Complete top's javascript exercise 05, 06 ,07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+
+    if (firstNum > 0 && secondNum > 0 && Number.isInteger(firstNum) && Number.isInteger(secondNum)) {
+        let sum = 0;
+        if (firstNum > secondNum) {
+            let temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+        }
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else return 'ERROR'
 
 };
 
